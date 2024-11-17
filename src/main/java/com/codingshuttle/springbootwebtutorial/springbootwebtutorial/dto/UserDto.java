e49999d0bc1bf5122509500915ebd1dda98db447
Suman Dey <04sumandey@gmail.com>
2024-11-17 11:44:01 +0530

    feat(add): First Commit

---
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private String FirstName;
+    private String LastName;
+}
